900afc8401adc284a01a18becf5210ea8631d3f3
Luis Fernando Cruz Carrillo <lfcruz@udes.edu.mx>
2020-07-31 16:21:33 -0500

    First

---
+++ app/htdocs/index.php
@@ -0,0 +1,174 @@
+<?php
+
+$name = 'Fernando Cruz';
+$mail = 'lfcruz@udes.edu.mx';
+$phone = '54654132135';
+$linkedIn = 'https://www.linkedin.com/';
+$twitter = '@fernando_cruz';
+
+$jobs = [
+  [
+    'title' => 'PHP dev',
+    'show' => true, 
+    'description' => 'PHP (acrónimo recursivo de PHP: Hypertext Preprocessor) es un lenguaje de código abierto muy popular especialmente adecuado para el desarrollo web y que puede ser incrustado en HTML.'
+  ],
+  [
+    'title' => 'Python dev',
+    'description' => 'Python is powerful... and fast; plays well with others; runs everywhere; is friendly & easy to learn; is Open.',
+  ],
+  [
+    'title' => 'DevOps',
+    'description' => 'DevOps (acrónimo inglés de development -desarrollo- y operations -operaciones-) es una práctica de ingeniería de software que tiene como objetivo unificar el desarrollo de software (Dev) y la operación del software (Ops). La principal característica del movimiento DevOps es defender enérgicamente la automatización y el monitoreo en todos los pasos de la construcción del software, desde la integración, las pruebas, la liberación hasta la implementación y la administración de la infraestructura. DevOps apunta a ciclos de desarrollo más cortos, mayor frecuencia de implementación, lanzamientos más confiables, en estrecha alineación con los objetivos comerciales.',
+  ],
+  [
+    'title' => 'Node dev',
+    'description' => 'Python is powerful... and fast; plays well with others; runs everywhere; is friendly & easy to learn; is Open.',
+  ],
+  [
+    'title' => 'VueJS dev',
+    'description' => 'Python is powerful... and fast; plays well with others; runs everywhere; is friendly & easy to learn; is Open.',
+  ],
+];
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+
+  <title>Resume</title>
+</head>
+
+<body>
+  <div class="container">
+    <div id="resume-header" class="row">
+      <div class="col-3">
+        <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
+      </div>
+      <div class="col">
+        <h1><?php echo($name) ?></h1>
+        <h2>PHP Developer</h2>
+        <ul>
+          <li>Mail: <?php echo($name); ?></li>
+          <li>Phone: <?php echo($phone); ?></li>
+          <li>LinkedIn: <?php echo($linkedIn); ?></li>
+          <li>Twitter: <?php echo($twitter); ?></li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <h2 class="border-bottom-gray" >Carrer Summary</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div>
+          <h3 class="border-bottom-gray" >Work Experience</h3>
+          <ul>
+            <?php
+              for ($idx=0; $idx < count($jobs); $idx++) { 
+            ?>
+                <li class="work-position">
+                  <h5> <?php echo($jobs[$idx]['title']) ?> </h5>
+                  <p><?php echo($jobs[$idx]['description']) ?></p>
+                  <strong>Achievements:</strong>
+                  <ul>
+                    <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
+                    <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
+                    <li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>
+                  </ul>
+                </li>
+            <?php
+              }
+            ?>
+           
+          </ul>
+        </div>
+        <div>
+            <h3 class="border-bottom-gray">Projects</h3>
+            <div class="project">
+                <h5>Project X</h5>
+                <div class="row">
+                    <div class="col-3">
+                        <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
+                      </div>
+                      <div class="col">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum corporis at accusamus quisquam hic quos vel? Tenetur, ullam veniam consequatur esse quod cum, quam cupiditate assumenda natus maiores aperiam.</p>
+                        <strong>Technologies used:</strong>
+                        <span class="badge badge-secondary">PHP</span>
+                        <span class="badge badge-secondary">HTML</span>
+                        <span class="badge badge-secondary">CSS</span>
+                      </div>
+                </div>
+            </div>
+            <div class="project">
+                <h5>Project X</h5>
+                <div class="row">
+                    <div class="col-3">
+                        <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
+                      </div>
+                      <div class="col">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum corporis at accusamus quisquam hic quos vel? Tenetur, ullam veniam consequatur esse quod cum, quam cupiditate assumenda natus maiores aperiam.</p>
+                        <strong>Technologies used:</strong>
+                        <span class="badge badge-secondary">PHP</span>
+                        <span class="badge badge-secondary">HTML</span>
+                        <span class="badge badge-secondary">CSS</span>
+                      </div>
+                </div>
+            </div>
+          </div>
+      </div>
+      <div class="col-3">
+        <h3 class="border-bottom-gray" >Skills & Tools</h3>
+        <h4>Backend</h4>
+        <ul>
+          <li>PHP</li>
+        </ul>
+        <h4>Frontend</h4>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+        </ul>
+        <h4>Frameworks</h4>
+        <ul>
+          <li>Laravel</li>
+          <li>Bootstrap</li>
+        </ul>
+        <h3 class="border-bottom-gray" >Languages</h3>
+        <ul>
+          <li>Spanish</li>
+          <li>English</li>
+        </ul>
+      </div>
+    </div>
+    <div id="resume-footer" class="row">
+      <div class="col">
+          Designed by @hectorbenitez
+      </div>
+    </div>
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
